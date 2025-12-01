bc41b4599bd173452c725b25e49baef999ba7e56
Ilya Bibikov <bibikov.ilya@gmail.com>
2025-12-01 12:38:30 +0300

    Add PriorbankItem create and delete. Cleanup priorbank env vars.

---
+++ app/controllers/priorbank_items_controller.rb
@@ -0,0 +1,59 @@
+class PriorbankItemsController < ApplicationController
+  before_action :set_priorbank_item, only: [ :destroy ]
+
+  def create
+    login = priorbank_params[:login]
+    password = priorbank_params[:password]
+    name = priorbank_params[:name].presence || "PriorBank Connection"
+
+    return render_error(t(".errors.blank_credentials")) if login.blank? || password.blank?
+
+    begin
+      Current.family.priorbank_items.create!(name:, login:, password:)
+      flash.now[:notice] = t(".success")
+      render_priorbank_panel_stream
+    rescue => e
+      Rails.logger.error("Priorbank connection error: #{e.message}")
+      render_error(t(".errors.unexpected", error: e.message))
+    end
+  end
+
+  def destroy
+    @priorbank_item.destroy_later
+    flash.now[:notice] = t(".success")
+    render_priorbank_panel_stream
+  end
+
+  private
+
+    def set_priorbank_item
+      @priorbank_item = Current.family.priorbank_items.find(params[:id])
+    end
+
+    def priorbank_params
+      params.require(:priorbank_item).permit(:name, :login, :password)
+    end
+
+    def render_priorbank_panel_stream
+      @priorbank_item = PriorbankItem.new
+      @priorbank_items = Current.family.priorbank_items.ordered
+
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.replace(
+              "priorbank-providers-panel",
+              partial: "settings/providers/priorbank_panel"
+            ), *flash_notification_stream_items
+          ]
+        end
+      end
+    end
+
+    def render_error(message)
+      @error_message = message
+
+      render_priorbank_panel_stream
+      response.status = :unprocessable_entity
+    end
+end
--- config/initializers/priorbank.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.configure do
-  if ENV["PRIORBANK_LOGIN"].present? && ENV["PRIORBANK_PASSWORD"].present?
-    config.prior = Struct.new(:login, :password).new(
-      ENV.fetch("PRIORBANK_LOGIN") { Setting.priorbank_login },
-      ENV.fetch("PRIORBANK_PASSWORD") { Setting.priorbank_password }
-    )
-  end
-end
